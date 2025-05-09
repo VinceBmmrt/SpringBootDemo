f7db441a320c067b8f93983c72fb0ec2b9f77530
VinceBmmrt <vincebommert75@gmail.com>
2025-05-09 17:11:22 +0200

    Repository Layer

--- src/main/java/com/telusko/app/service/LaptopService.java
+++ src/main/java/com/telusko/app/service/LaptopService.java
@@ -1,12 +1,18 @@
 package com.telusko.app.service;
 
+import com.telusko.app.LaptopRepository;
 import com.telusko.app.model.Laptop;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LaptopService {
+
+    @Autowired
+    private LaptopRepository repo;
+
     public void addLaptop(Laptop lap) {
-        System.out.println("Method called");
+        repo.save(lap);
     }
 
     public boolean isGoodForProgramming(Laptop lap){
